559146682c1dae7bea8b42c9b95c26307f9a7715
Mageus <mateusvish90@gmail.com>
2026-05-06 22:01:48 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,76 @@
+def main():
+    while True:
+        print("Escolha qual operação deseja realizar:")
+        print("1 - Adição")
+        print("2 - Subtração")
+        print("3 - Multiplicação")
+        print("4 - Divisão")
+        print("5 - Potenciação")
+        escolha=input("Digite o número da operação: ")
+        if escolha.isdigit()!=True:
+            print("Por favor digite um número.")
+            cont=input("Continuar? (s/n): ")
+            cont=cont.lower()
+            if cont!=("s"):
+                print("Ok!")
+                break
+                
+        elif int(escolha) in [1,2,3,4,5]:
+            num1=float(input("Digite o primeiro número: "))
+            num2=float(input("Digite o segundo número: "))
+            if calculo(int(escolha), num1, num2)!=None:
+                print("O resultado é:" , calculo(int(escolha), num1, num2))
+                cont=input("Continuar? (s/n): ")
+                cont=cont.lower()
+                if cont!=("s"):
+                    print("Ok!")
+                    break
+            else:
+                cont=input("Continuar? (s/n): ")
+                cont=cont.lower()
+                if cont!=("s"):
+                    print("Ok!")
+                    break
+                
+                
+        
+           
+            
+        else:
+            print("Escolha um número válido.")
+            cont=input("Continuar? (s/n): ")
+            cont=cont.lower()
+            if cont!=("s"):
+                print("Ok!")
+                break
+                
+        
+            
+            
+                
+def calculo(operacao, x , y):
+    
+    if operacao==1:
+        return(round(x+y,2))
+    elif operacao==2:
+        return(round(x-y,2))
+    elif operacao==3:
+        return(round(x*y,2))
+    elif operacao==4:
+        if y==0:
+            print("Não é possível dividir por 0")
+            return None
+        else:
+            return(round(x/y,2))
+    elif operacao==5:
+        return(round(x**y,2))
+    
+                
+                
+        
+        
+        
+            
+print("Bem vindo a calculadora!")
+main()
+        
